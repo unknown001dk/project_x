0315c048b32ee1d5aeaa0f53973ec90c1c46548b
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-06-22 12:15:07 +0530

    completed user regesiter Controller

--- api/controllers/User-controller.js
+++ api/controllers/User-controller.js
@@ -1,8 +1,44 @@
+import mongoose from "mongoose";
+import User from "../models/User-model.js";
 
-export const registerUser = (req, res) => {
-  res.status(200).json({ 
-    message: 'Register sucessfully' 
+export const registerUser = async(req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name ||!email ||!password) {
+    return res.status(400).json({ 
+      message: 'Please fill all fields',
+      status: 400,
+      success: false
+    });
+  }
+
+  if( email ) {
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ 
+        message: 'Email already exists',
+        status: 400,
+        success: false
+      });
+    }
+  }
+
+  const newUser = new User({
+    name,
+    email,
+    password,
   });
+
+  try {
+    await newUser.save();
+    return res.status(200).json({ 
+      message: 'Register sucessfully',
+      status: 201,
+      success: true
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const loginUser = (req, res) => {
