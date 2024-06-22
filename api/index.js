097aeb28f2f92379b2a69f7e54516d0eb8ea19d1
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-06-22 10:44:52 +0530

    creating server

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
