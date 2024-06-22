37f2dfd075d9727979e3e3a07463587632218c11
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-06-22 10:56:24 +0530

    add routes and create pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NotFound from './utils/NotFound';
+import Register from './pages/users/Register';
+import Login from './pages/users/Login';
+import Home from './pages/Home';
+import Profile from './pages/users/Profile';
 
 function App() {
   return (
-    <div className='bg-red-800'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='profile' element={<Profile />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
