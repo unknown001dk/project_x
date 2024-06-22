b8d829c6e48946865144905de35195eda0b7fec5
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-06-22 11:07:22 +0530

    added register page and login page UI

--- frontend/src/pages/users/Login.jsx
+++ frontend/src/pages/users/Login.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='p-3 max-w-lg mx-auto mt-48 md:mt-44'>
+    <h1 className='text-3xl text-center font-semibold my-7'>Login</h1>
+    <form className='flex flex-col gap-4 '>
+      <input
+        className='bg-slate-100 p-3 rounded-lg'
+        type="email"
+        placeholder='Email'
+        id='email' />
+      <input
+        className='bg-slate-100 p-3 rounded-lg'
+        type="password"
+        placeholder='Password'
+        id='password' />
+        <button
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+            Login
+        </button>
+    </form>
+    <div className='flex gap-2 mt-5'>
+      <p>Not have an account?</p>
+      <Link to='/register'>
+        <span className='text-blue-500 '>Register</span>
+      </Link>
+    </div>
+    </div>
   )
 }
 
--- frontend/src/pages/users/Register.jsx
+++ frontend/src/pages/users/Register.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='p-3 max-w-lg mx-auto mt-48 md:mt-44'>
+    <h1 className='text-3xl text-center font-semibold my-7'>Register</h1>
+    <form className='flex flex-col gap-4 '>
+      <input
+        className='bg-slate-100 p-3 rounded-lg'
+        type="text"
+        placeholder='Username'
+        id='username' />
+      <input
+        className='bg-slate-100 p-3 rounded-lg'
+        type="email"
+        placeholder='Email'
+        id='email' />
+      <input
+        className='bg-slate-100 p-3 rounded-lg'
+        type="password"
+        placeholder='Password'
+        id='password' />
+        <button
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+            Register
+        </button>
+    </form>
+    <div className='flex gap-2 mt-5'>
+      <p>Have an account?</p>
+      <Link to='/login'>
+        <span className='text-blue-500 '>Login</span>
+      </Link>
+    </div>
+    </div>
   )
 }
 
